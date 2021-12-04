694ee654dd082e36b0d1c6ca7ad9e51c00a33906
Rahi Akela <rahi.akela@yahoo.com>
2021-12-04 12:40:18 +0530

    adding some code

---
+++ probability-for-data-science/1-mathematical-background/1_infinite_series.py
@@ -0,0 +1,10 @@
+# Python code to generate a geometric sequence
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+p = 1 / 2
+n = np.arange(0, 10)
+X = np.power(p, n)
+plt.bar(n, X)
+plt.show()
---
+++ probability-for-data-science/1-mathematical-background/2_binomial_series.py
@@ -0,0 +1,9 @@
+from scipy.special import comb, factorial
+
+
+# Python code to compute (N choose K) and K!
+n = 10
+k = 2
+
+print(comb(n, k))
+print(factorial(k))
